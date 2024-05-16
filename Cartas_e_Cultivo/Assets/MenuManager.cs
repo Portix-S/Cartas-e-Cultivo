9a7ca9ad7100a293b46e834685d828cb48b529e6
Naga <brunoyui@estudante.ufscar.br>
2024-05-16 17:01:49 -0300

    Fades

--- Cartas_e_Cultivo/Assets/MenuManager.cs
+++ Cartas_e_Cultivo/Assets/MenuManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
+   
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
     }
 
     public void Exit()
@@ -19,5 +20,6 @@ public class MenuManager : MonoBehaviour
     public void PlayAgain()
     {
         SceneManager.LoadScene("Menu");
+        
     }
 }
